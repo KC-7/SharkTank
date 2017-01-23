7e6b1d8fd8d13c58132fa4adcfbf1d6c776bbfc8
KC7 <kartikchugh.kc@gmail.com>
2017-01-22 20:16:34 -0500

    Continues progress

---
+++ app/src/main/java/kkmp/sharktank/C_Submitted_Info.java
@@ -0,0 +1,14 @@
+package kkmp.sharktank;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class C_Submitted_Info extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.c_submitted_info);
+    }
+
+}
--- app/src/main/java/kkmp/sharktank/Main.java
+++ app/src/main/java/kkmp/sharktank/Main.java
@@ -1,13 +1,25 @@
 package kkmp.sharktank;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
     }
+
+    public void clickedButton_caregiver(View view) {
+        final Intent intent = new Intent(this, C_Submit_Info.class);
+        startActivity(intent);
+    }
+
+    public void clickedButton_recipient(View view) {
+
+    }
+
 }
