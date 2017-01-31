965bd8d32b23880c59d987ed3df34d21fefcd6e3
KC7 <kartikchugh.kc@gmail.com>
2017-01-31 01:43:50 -0500

    Made progress on selection screen for request/listing

--- app/src/main/java/kkmp/sharktank/C_Select_Request.java
+++ app/src/main/java/kkmp/sharktank/C_Select_Request.java
@@ -1,7 +1,11 @@
 package kkmp.sharktank;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 /**
  * Created by kchugh on 1/31/2017 at 12:32 AM
@@ -12,6 +16,24 @@ public class C_Select_Request extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.c_select_request);
+
+        final Intent intent = getIntent();
+        final HashMap<String, String> requestMap = (HashMap<String, String>)intent.getSerializableExtra("requestMap");
+
+        TextView title = (TextView)findViewById(R.id.request_title);
+        TextView tags = (TextView)findViewById(R.id.request_tags);
+        TextView timings = (TextView)findViewById(R.id.request_timings);
+        TextView comments = (TextView)findViewById(R.id.request_comments);
+
+        String titleString = requestMap.get("title");
+        String tagsString = "Tags: " + requestMap.get("tags").trim().replace(" ",", ");
+        String timingsString = "Timings: " + requestMap.get("timings");
+        String commentsString = "Details: " + requestMap.get("comments");
+
+        title.setText(titleString);
+        tags.setText(tagsString);
+        timings.setText(timingsString);
+        comments.setText(commentsString);
     }
 
 }
--- app/src/main/java/kkmp/sharktank/R_Select_Listing.java
+++ app/src/main/java/kkmp/sharktank/R_Select_Listing.java
@@ -1,7 +1,11 @@
 package kkmp.sharktank;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 /**
  * Created by kchugh on 1/31/2017 at 12:50 AM
@@ -12,6 +16,24 @@ public class R_Select_Listing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.r_select_listing);
+
+        final Intent intent = getIntent();
+        final HashMap<String, String> listingMap = (HashMap<String, String>)intent.getSerializableExtra("listingMap");
+
+        TextView title = (TextView)findViewById(R.id.listing_title);
+        TextView tags = (TextView)findViewById(R.id.listing_tags);
+        TextView timings = (TextView)findViewById(R.id.listing_timings);
+        TextView comments = (TextView)findViewById(R.id.listing_comments);
+
+        String titleString = listingMap.get("title");
+        String tagsString = "Tags: " + listingMap.get("tags").trim().replace(" ",", ");
+        String timingsString = "Timings: " + listingMap.get("timings");
+        String commentsString = "Details: " + listingMap.get("comments");
+
+        title.setText(titleString);
+        tags.setText(tagsString);
+        timings.setText(timingsString);
+        comments.setText(commentsString);
     }
 
 }
